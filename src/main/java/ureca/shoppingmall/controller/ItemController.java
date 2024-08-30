48fae3e05ad9f566704acf51bb57cdeabe10a893
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-08-30 17:37:07 +0900

    :sparkles: Controller 틀 작성

--- src/main/java/ureca/shoppingmall/controller/ItemController.java
+++ src/main/java/ureca/shoppingmall/controller/ItemController.java
@@ -1,4 +1,10 @@
 package ureca.shoppingmall.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/items")
 public class ItemController {
+
 }
