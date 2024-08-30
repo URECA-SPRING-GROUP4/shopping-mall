65401952f4fb9b625b0db7856b3d6faca905ad84
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-08-30 17:20:10 +0900

    :sparkles: repository 생성

--- src/main/java/ureca/shoppingmall/repository/ItemRepository.java
+++ src/main/java/ureca/shoppingmall/repository/ItemRepository.java
@@ -1,4 +1,22 @@
 package ureca.shoppingmall.repository;
 
-public interface ItemRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import ureca.shoppingmall.domain.Item.Item;
+import ureca.shoppingmall.dto.ItemDto;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+    Item findByName(String name);
+
+    // 상품 조회
+    List<ItemDto> findAllByOrderByIdDesc();
+    Item findById(long id);
+
+    // 재고 확인
+    @Query("select i from Item i where i.id = :id and i.stockQuantity >= :quantity")
+    List<Item> findByStockQuantity(@Param("id") long id, @Param("quantity") float quantity);
+
 }
--- src/main/java/ureca/shoppingmall/repository/OrderRepository.java
+++ src/main/java/ureca/shoppingmall/repository/OrderRepository.java
@@ -1,4 +1,23 @@
 package ureca.shoppingmall.repository;
 
-public interface OrderRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import ureca.shoppingmall.common.enums.OrderStatus;
+import ureca.shoppingmall.domain.order.Order;
+import ureca.shoppingmall.domain.user.User;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    // 사용자 별 주문 조회
+    List<Order> findByUser(User user);
+
+    // 주문 상태별 조회
+    List<Order> findByUser(User user, OrderStatus status);
+
+    // 주문과 주문 아이템을 함께 조회
+    @Query("select o from Order o join fetch o.orderItems oi join fetch oi.item where o.id = :orderId")
+    Order findOrderWithItems(@Param("orderId") Long orderId);
 }
