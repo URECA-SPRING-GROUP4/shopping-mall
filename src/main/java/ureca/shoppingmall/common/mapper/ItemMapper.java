60b56747a6ad7c1ba53a63ad21e6159dbe7591e3
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-08-30 17:34:56 +0900

    :sparkles: ItemMapper 작성

---
+++ src/main/java/ureca/shoppingmall/common/mapper/ItemMapper.java
@@ -0,0 +1,19 @@
+package ureca.shoppingmall.common.mapper;
+
+
+import ureca.shoppingmall.domain.Item.Item;
+import ureca.shoppingmall.dto.ItemDto;
+
+public class ItemMapper {
+
+    public static ItemDto toDto(Item item) {
+        ItemDto dto = new ItemDto();
+        dto.setId(item.getId());
+        dto.setName(item.getName());
+        dto.setPrice(item.getPrice());
+        dto.setDescription(item.getDescription());
+        dto.setStockQuantity(String.valueOf(item.getStockQuantity()));
+        return dto;
+    }
+}
+
